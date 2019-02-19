6df4893c2c697669244dfeb2eecb00c2bd147740
hughlaunet <hughlaunet@gmail.com>
2019-02-19 21:46:53 +0800

    dpems

---
+++ DPEMS/DEPMS.Themes/Manifest.cs
@@ -0,0 +1,9 @@
+﻿using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "DPEThemes.OrchardCore",
+    Author = "L",
+    Website = "https://dapange.com",
+    Version = "0.0.1",
+    Description = "The TemplateTheme."
+)]
