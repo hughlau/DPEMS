6fc5dd3f0128acb12ab17b3fcbb05483f704f1dd
hughlau <hughlaunet@gmail.com>
2019-02-20 17:20:13 +0800

    delete

--- DEPMS.Themes/Manifest.cs
+++
@@ -1,9 +0,0 @@
-﻿using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "DPEThemes.OrchardCore",
-    Author = "L",
-    Website = "https://dapange.com",
-    Version = "0.0.1",
-    Description = "The TemplateTheme."
-)]
