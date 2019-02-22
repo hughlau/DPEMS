12c6bbaf148a9fa53881fc2e6ef1050bf742da52
hughlau <hughlaunet@gmail.com>
2019-02-22 15:39:45 +0800

    delete all

--- Themes/DPEMS.FirstTheme/Manifest.cs
+++
@@ -1,9 +0,0 @@
-﻿using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "DPEMS.FirstTheme",
-    Author = "L",
-    Website = "https://dapange.com",
-    Version = "0.0.1",
-    Description = "The FirstTheme."
-)]
