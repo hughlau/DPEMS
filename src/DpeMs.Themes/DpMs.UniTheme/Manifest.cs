47b2eda6529b9df811c8433d32848ad5d91d7bdc
hughlau <hughlaunet@gmail.com>
2019-02-25 17:39:28 +0800

    unithem

---
+++ src/DpeMs.Themes/DpMs.UniTheme/Manifest.cs
@@ -0,0 +1,9 @@
+﻿using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "DpeMs.UniTheme",
+    Author = "L",
+    Website = "https://orchardproject.net",
+    Version = "0.0.1",
+    Description = "The UniTheme."
+)]
