20759bc0d32c62ff874207bc87cdf8b45652da86
hughlau <hughlaunet@gmail.com>
2019-02-26 17:54:32 +0800

    sec

---
+++ src/DpeMs.Themes/DpeMs.RazorTheme/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "DpeMs.RazorTheme",
+    Author = "L",
+    Website = "http://orchardproject.net",
+    Version = "2.0.0",
+    Description = "The default Admin theme.",
+    Tags = new [] { "admin" }
+)]
