5e41dc5936ff715781c20abf4a4c80aeae2ca740
hughlau <hughlaunet@gmail.com>
2019-02-28 17:27:50 +0800

    cc

--- src/DpeMs.Web/DpeMs.Web/Startup.cs
+++ src/DpeMs.Web/DpeMs.Web/Startup.cs
@@ -19,7 +19,6 @@ namespace DpeMs.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(HtmlEncoder.Create(UnicodeRanges.All));
             services.AddScoped<IResourceManifestProvider, ResourceManifestBuilder>();
 
             services.AddOrchardCms();
